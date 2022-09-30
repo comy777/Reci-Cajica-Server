edd5bef3d70705ba02dbcca5119fb7c8bcc41209
Sebastian Carrillo <carrillosebastian1@gmail.com>
2022-09-30 10:57:55 -0500

    Edit send email user

--- dist/emails/config.js
+++ dist/emails/config.js
@@ -26,11 +26,21 @@ const transporter = nodemailer_1.default.createTransport({
 const sendEmailUser = (email) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         yield transporter.sendMail({
-            from: '"Cajica Recicla"',
+            from: '"Reci-Cajica"',
             to: email,
-            subject: "Peticion recibida",
-            text: "Nos pondremos en contacto con usted para darle mas informacion al respecto de cajica recicla",
-            html: "<b>Hello world?</b>", // html body
+            subject: "Su peticion ha sido recibida",
+            text: "Nos pondremos en contacto con usted para darle mas informacion al respecto de cajica Reci-Cajica",
+            html: `
+        <div>
+          <h1>Reci-Cajica</h1>
+          <div>
+            <p>Reciba un cordial saludo de parte nuestra, es un gusto ver su interes por nuestro servicio, el cual busca brindar información importante acerca del reciclaje en Cajica, es por ello que lo invitamos a continuar este proceso de formación.
+            <br/>
+            Mas adelante nos comunicaremos con usted, para brindarle mas información, muchas gracias.
+            </p>
+          </div>
+        </div>
+      `,
         });
         return true;
     }
