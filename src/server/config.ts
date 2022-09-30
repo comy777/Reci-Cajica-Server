159a457f955d932a0efacc5eac75371ca6cbfddb
Sebastian Carrillo <carrillosebastian1@gmail.com>
2022-09-30 09:40:15 -0500

    Added type to port

--- src/server/config.ts
+++ src/server/config.ts
@@ -7,7 +7,7 @@ import appRoutes from "../routes/routes";
 
 class Server {
   app: Express = express();
-  port: string;
+  port: string | number;
   path: {
     home: string;
   };
@@ -15,7 +15,7 @@ class Server {
   constructor() {
     this.db();
     this.app = this.app;
-    this.port = process.env.PORT || "1337";
+    this.port = process.env.PORT || 1337;
     this.middlewares();
     this.path = {
       home: "/",
